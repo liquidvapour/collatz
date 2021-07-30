6dcb0eb6b5e97ca829aa6164f21332b9b4adb316
liquidvapour <liquidvapour@gmail.com>
2021-07-30 22:31:32 +0100

    first real failing tests

---
+++ src/Collatz.Tests/CollatzTests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Collatz.Tests
+{
+    public class CollatzTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void OneWillReturnOne()
+        {
+            Program.Hotop(1).Should().Be(1);
+        }
+
+        [Test]
+        public void FiveWillReturnFive()
+        {
+            Program.Hotop(5).Should().Be(5);
+        }
+    }
+}
---
+++ src/Collatz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Collatz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int Hotop(int v)
+        {
+            return 1;
+        }
+    }
+}
