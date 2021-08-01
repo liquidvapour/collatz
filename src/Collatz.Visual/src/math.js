53ef98036f896cecc64f1932d7e8c5cce9edba6b
liquidvapour <liquidvapour@gmail.com>
2021-08-01 23:47:26 +0100

    color lerp

--- src/Collatz.Visual/src/math.js
+++ src/Collatz.Visual/src/math.js
@@ -1,8 +1,12 @@
- // Precise method, which guarantees v = v1 when t = 1.
- export const lerp = (v0, v1, t) => (1 - t) * v0 + t * v1;
+import lerp from "@sunify/lerp-color";
+// Precise method, which guarantees v = v1 when t = 1.
+export const lerpInt = (v0, v1, t) => (1 - t) * v0 + t * v1;
 
 export const randomBetween = (min, max) => (Math.random() * (max - min)) + min;
 
-export const randomColor = () => `rgb(${randomBetween(0, 255)},${randomBetween(0, 255)},${randomBetween(0, 255)})`;
+export const randomColor = () => { 
+    return lerp('#3D0E4F', '#A139C6', Math.random()); 
+    //return `rgb(${randomBetween(0, 255)},${randomBetween(0, 255)},${randomBetween(0, 255)})`
+};
 
 export const degToRad = (deg) => deg * Math.PI / 180;
