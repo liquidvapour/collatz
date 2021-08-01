69946c59cbe7e492c3328c4885653a737e0ad438
liquidvapour <liquidvapour@gmail.com>
2021-08-01 01:15:09 +0100

    render a branch

--- src/Collatz.Visual/src/rendering/collatzTree.js
+++ src/Collatz.Visual/src/rendering/collatzTree.js
@@ -18,15 +18,47 @@ const hotpoInternal = (v, count, accume) => {
 }
 
 export const create = () => ({
-    tree: hotpoInternal(16, 0)
+    tree: hotpoInternal(17, 0)
 });
 
-export const draw = (ctx, collatz) => {
-    console.log(collatz);
-    ctx.save();
-    ctx.fillStyle = "#F48C25";
-    ctx.translate(ctx.canvas.width/2, ctx.canvas.height/2);
-    ctx.scale(10, 50);
+const drawRect = (ctx, w, h) => {
+    ctx.save()   
+    ctx.scale(w, h);
     ctx.fillRect(0, 0, 1, 1);
     ctx.restore();
+};
+
+//const degToRad = (deg) => deg * Math.PI / 180;
+
+const drawBranch = (ctx, branch) => {
+    console.log(branch);
+    ctx.save();
+    ctx.fillStyle = "#F48C25";
+    ctx.translate(ctx.canvas.width/2, ctx.canvas.height);
+    drawRect(ctx,10, -50);
+    const revBranch = [...branch];
+    revBranch.reverse();
+    let last = 1;
+    for (let i = 0; i < revBranch.length; i++) {
+        const current = revBranch[i];
+        const diff = last - current;
+        let x = 0;
+        if (diff > 0) {
+            x = 20; 
+            //ctx.rotate(degToRad(315));
+        } else if (diff < 0) {
+            x = -20;
+            //ctx.rotate(degToRad(225));
+        }
+        ctx.translate(x, 0);
+        drawRect(ctx, 10, -50);
+        ctx.translate(0, -50);
+        last = current;
+    }
+    ctx.restore();
+
+};
+
+export const draw = (ctx, collatz) => {
+    drawBranch(ctx, collatz.tree);
 };
